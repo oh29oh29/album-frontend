cc993854ead97e1d6a7b47be3f0cff6becef6387
oh29oh29 <hyukjae89@gmail.com>
2019-02-09 23:22:24 +0900

    vue cli upgrade

---
+++ src/router.js
@@ -0,0 +1,53 @@
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import Home from './views/Home.vue'
+//
+// Vue.use(Router)
+//
+// export default new Router({
+//   mode: 'history',
+//   base: process.env.BASE_URL,
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: Home
+//     },
+//     {
+//       path: '/about',
+//       name: 'about',
+//       // route level code-splitting
+//       // this generates a separate chunk (about.[hash].js) for this route
+//       // which is lazy-loaded when the route is visited.
+//       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+//     }
+//   ]
+// })
+
+import Vue from 'vue'
+import Router from 'vue-router'
+import Category from '@/components/album/Category'
+import List from '@/components/album/List'
+import Detail from '@/components/album/Detail'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'category',
+      component: Category
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: List
+    },
+    {
+      path: '/detail',
+      name: 'detail',
+      component: Detail
+    }
+  ]
+})
